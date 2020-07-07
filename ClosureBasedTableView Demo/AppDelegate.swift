e93476f6c1cc68e92db94ec388d10cad9959a9e1
Steve Sheets <magesteve@mac.com>
2020-07-06 23:02:27 -0400

    Initial Commit

---
+++ ClosureBasedTableView Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ClosureBasedTableView Demo
+//
+//  Created by Steve Sheets on 7/6/20.
+//  Copyright © 2020 Steve Sheets. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClosureBasedTableView Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClosureBasedTableView Demo
+//
+//  Created by Steve Sheets on 7/6/20.
+//  Copyright © 2020 Steve Sheets. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
